aee12f13939f0e3a9b5c9d4a8f19c7675ebc28d9
Shukhrat Nurov <shuhratnurov32@gmail.com>
2022-10-17 15:36:26 +0500

    --

--- UnsplashPhotoPicker-Test/ViewController.swift
+++ UnsplashPhotoPicker-Test/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let testLabel:UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Test"
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addSubview(testLabel)
+        
+        
+        testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
 
